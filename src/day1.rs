4bfa9c25800f26385d62ccd0b6ea6fc993189b74
Romain DÉOUX <romain.deoux@proton.me>
2023-12-02 20:07:39 +0100

    second part of the solution of the day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,12 +1,51 @@
 const INPUT: &str = include_str!("day1.txt");
 
-pub fn result() -> u32 {
+pub fn first_half() -> u32 {
     // Split the input into lines.
     INPUT
         .trim()
         .lines()
         // For each line, get only the digits.
         .map(|line| line.chars().filter_map(|c| c.to_digit(10)))
+        .map(|mut digits| {
+            // For each list of digits, get the first one...
+            let first = digits
+                .next()
+                .expect("There should always be at least one digit");
+            // ...and the last one (if it exists)
+            let last = digits.last().unwrap_or(first);
+            // Convert to a single number.
+            first * 10 + last
+        })
+        // Returns the sum.
+        .sum()
+}
+
+pub fn second_half() -> u32 {
+    const DIGITS: [&str; 10] = [
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    // Split the input into lines.
+    INPUT
+        .trim()
+        .lines()
+        // Convert each line into an list of digits.
+        .map(|line| {
+            // Convert the line into a list slices.
+            line.char_indices().filter_map(|(index, c)| {
+                // Try to convert from a single digit...
+                c.to_digit(10)
+                    // ...or try to convert from a string.
+                    .or_else(|| {
+                        DIGITS
+                            .iter()
+                            .enumerate()
+                            .find(|(_digit, &name)| line[index..].starts_with(name))
+                            .and_then(|(digit, _name)| u32::try_from(digit).ok())
+                    })
+            })
+        })
         .map(|mut digits| {
             // For each list of digits, get the first one...
             let first = digits
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day1;
 
 fn main() {
-    println!("Day 1: {}", day1::result())
+    println!("Day 1 (first part): {}", day1::first_half());
+    println!("Day 1 (second part): {}", day1::second_half());
 }
